9b2edb4a3f232f3253c418dcbd50130a50126bcb
eallenOP <elise.allen@op.ac.nz>
2025-05-08 16:17:56 +1200

    Place components on home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Footer from "$lib/Footer.svelte";
+    import Card from "$lib/Card.svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Card />
+<Card />
+<Card />
+<Card />
+<Card />
+<Footer />
+
+<style>
+    :global(body) {
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+</style>
