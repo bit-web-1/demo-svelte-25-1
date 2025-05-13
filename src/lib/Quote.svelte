86e242140845e98948d1dfb487215d6fa003a6d8
eallenOP <elise.allen@op.ac.nz>
2025-05-13 16:07:35 +1200

    Style the quotes

--- src/lib/Quote.svelte
+++ src/lib/Quote.svelte
@@ -1,16 +1,30 @@
 <script>
-    let { quote } = $props();
+  let { quote } = $props();
 </script>
 
 <div>
-    <blockquote cite={quote.citeUrl}>
-      <p>
-        {quote.quoteText}
-      </p>
-    </blockquote>
-    <p>—{quote.author}, <cite>{quote.publication}</cite></p>
-  </div>
+  <blockquote cite={quote.citeUrl}>
+    <p>
+      {quote.quoteText}
+    </p>
+  </blockquote>
+  <p>—{quote.author}, <cite>{quote.publication}</cite></p>
+</div>
 
 <style>
-    
+  div {
+    border: 1px dashed silver;
+    border-radius: 10px;
+    background-color: peachpuff;
+    margin: 1em;
+    padding: 1em;
+  }
+  blockquote {
+    border-left: 0.5em solid tomato;
+    padding-left: 0.5em;
+  }
+  blockquote + p {
+    margin-left: 3em;
+    color: tomato;
+  }
 </style>
