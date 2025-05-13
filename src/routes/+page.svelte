509f5e63684f3b7a030f806b739b29fe2d142414
ealleOP <elise.allen@op.ac.nz>
2025-05-13 16:01:58 +1200

    Loop over array of quotes for many components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,35 +3,34 @@
     import Card from "$lib/Card.svelte";
     import Quote from "$lib/Quote.svelte";
 
-    let lorem =  {
-        quoteText: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, totam qui nulla accusantium incidunt odit quo nihil voluptatem voluptatum officiis, eum fugiat consectetur? Inventore, sed dicta sunt placeat dolore molestiae!",
-        author: "Cicero",
-        publication: "Lorem Ipsum",
-        citeUrl: "https://www.huxley.net/bnw/four.html"
+    let quotes = [
+        {
+            quoteText: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, totam qui nulla accusantium incidunt odit quo nihil voluptatem voluptatum officiis, eum fugiat consectetur? Inventore, sed dicta sunt placeat dolore molestiae!",
+            author: "Cicero",
+            publication: "Lorem Ipsum",
+            citeUrl: "https://www.huxley.net/bnw/four.html"
+        },
+        {
+            quoteText: "Words can be like X-rays, if you use them properly—they’ll go through anything. You read and you’re pierced.",
+            author: "Aldous Huxley",
+            publication: "Brave New World",
+            citeUrl: "https://www.huxley.net/bnw/four.html"
+        },
+        {
+            quoteText: "I'm Batman",
+            author: "Batman",
+            publication: "Dark Knight",
+            citeUrl: "https://www.huxley.net/bnw/four.html"
         }
-
-    let huxley =  {
-        quoteText: "Words can be like X-rays, if you use them properly—they’ll go through anything. You read and you’re pierced.",
-        author: "Aldous Huxley",
-        publication: "Brave New World",
-        citeUrl: "https://www.huxley.net/bnw/four.html"
-        }
-
-    let batman =  {
-        quoteText: "I'm Batman",
-        author: "Batman",
-        publication: "Dark Knight",
-        citeUrl: "https://www.huxley.net/bnw/four.html"
-        }
-
+    ];  
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<Quote quote={huxley} />
-<Quote quote={batman} />
-<Quote quote={lorem} />
+{#each quotes as quote}
+    <Quote {quote} />
+{/each}
 
 <section class="card-grid">
     <Card />
