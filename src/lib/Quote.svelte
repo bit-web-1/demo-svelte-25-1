937ddca39dc3b999351ef85d521cd407db4c1625
ealleOP <elise.allen@op.ac.nz>
2025-05-13 15:25:01 +1200

    Move quote text to an object

--- src/lib/Quote.svelte
+++ src/lib/Quote.svelte
@@ -1,12 +1,13 @@
 <script>
-
+    let quote = {
+        quoteText: "Words can be like X-rays, if you use them properly—they’ll go through anything. You read and you’re pierced."
+    }
 </script>
 
 <div>
     <blockquote cite="https://www.huxley.net/bnw/four.html">
       <p>
-        Words can be like X-rays, if you use them properly—they’ll go through
-        anything. You read and you’re pierced.
+        {quote.quoteText}
       </p>
     </blockquote>
     <p>—Aldous Huxley, <cite>Brave New World</cite></p>
