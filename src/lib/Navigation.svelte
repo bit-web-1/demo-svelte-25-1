127946522a6a3b322790f0d8409bca2d9fbe9f4f
eallenOP <elise.allen@op.ac.nz>
2025-05-15 15:41:51 +1200

    Create a blog landing page

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -5,4 +5,5 @@
 <nav>
     <a href="/">Home</a>
     <a href="/about">About</a>
+    <a href="/blog">Blog</a>
 </nav>
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    // This is the landing page for my blog
+    // It will contain a contents page with links to all my blog posts
+
+    import SiteHeader from "$lib/SiteHeader.svelte";
+</script>
+
+<SiteHeader />
+
+<h2>My Blog</h2>
+
+<!-- Links will go here -->
