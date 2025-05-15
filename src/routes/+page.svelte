79e1fec58f2114b10492a1b0605ac0e93bf2f276
eallenOP <elise.allen@op.ac.nz>
2025-05-15 15:35:19 +1200

    Shift the quotes to the about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,7 @@
 <script>
     import Footer from "$lib/Footer.svelte";
     import Card from "$lib/Card.svelte";
-    import Quote from "$lib/Quote.svelte";
-
-    let quotes = [
-        {
-            quoteText: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, totam qui nulla accusantium incidunt odit quo nihil voluptatem voluptatum officiis, eum fugiat consectetur? Inventore, sed dicta sunt placeat dolore molestiae!",
-            author: "Cicero",
-            publication: "Lorem Ipsum",
-            citeUrl: "https://www.huxley.net/bnw/four.html"
-        },
-        {
-            quoteText: "Words can be like X-rays, if you use them properly—they’ll go through anything. You read and you’re pierced.",
-            author: "Aldous Huxley",
-            publication: "Brave New World",
-            citeUrl: "https://www.huxley.net/bnw/four.html"
-        },
-        {
-            quoteText: "I'm Batman",
-            author: "Batman",
-            publication: "Dark Knight",
-            citeUrl: "https://www.huxley.net/bnw/four.html"
-        },
-        {
-            quoteText: "It is hard to fail but it is worse never to have tried to succeed.",
-            author: "Theodore Roosevelt",
-            publication: "",
-            citeUrl: "https://www.shutterfly.com/ideas/best-quotes-of-all-time/?msockid=3c99785b330c66ae0c8f6db0323e67db"
-        }
-    ];
+    import SiteHeader from "$lib/SiteHeader.svelte";
 
     let cards = [
         {
@@ -50,12 +23,7 @@
     ]
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-{#each quotes as quote}
-    <Quote {quote} />
-{/each}
+<SiteHeader />
 
 <section class="card-grid">
     {#each cards as card}
