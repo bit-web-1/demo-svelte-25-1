317c786bda1e81f3e17dcfc2a22b5f205f27bc7c
eallenOP <elise.allen@op.ac.nz>
2025-05-20 16:15:16 +1200

    Add the footer to the main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Footer from "$lib/Footer.svelte";
   import SiteHeader from "$lib/SiteHeader.svelte";
 
   let { children } = $props();
@@ -9,6 +10,9 @@
 
 <!-- This is where the page content will go -->
 {@render children()}
+
+<!-- The website footer -->
+<Footer />
 
 <style>
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,49 @@
 <script>
-    import Footer from "$lib/Footer.svelte";
-    import Card from "$lib/Card.svelte";
+  import Card from "$lib/Card.svelte";
 
-    let cards = [
-        {
-            title: "My Card",
-            user: {
-                userName: "@doggo",
-                img: "https://picsum.photos/id/237/200/200"
-            },
-            info: "information here"
-        },
-        {
-            title: "My Other Card",
-            user: {
-                userName: "@doggo2",
-                img: "https://picsum.photos/id/237/200/200"
-            },
-            info: "More kinds of information here"
-        }
-    ]
+  let cards = [
+    {
+      title: "My Card",
+      user: {
+        userName: "@doggo",
+        img: "https://picsum.photos/id/237/200/200",
+      },
+      info: "information here",
+    },
+    {
+      title: "My Other Card",
+      user: {
+        userName: "@doggo2",
+        img: "https://picsum.photos/id/237/200/200",
+      },
+      info: "More kinds of information here",
+    },
+  ];
 </script>
 
 <section class="card-grid">
-    {#each cards as card}
-        <Card cardData={card} />
-    {/each}
+  {#each cards as card}
+    <Card cardData={card} />
+  {/each}
 </section>
-<Footer />
 
 <style>
-    :global(body) {
-        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    }
-    .card-grid {
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
-    }
+  :global(body) {
+    font-family:
+      system-ui,
+      -apple-system,
+      BlinkMacSystemFont,
+      "Segoe UI",
+      Roboto,
+      Oxygen,
+      Ubuntu,
+      Cantarell,
+      "Open Sans",
+      "Helvetica Neue",
+      sans-serif;
+  }
+  .card-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+  }
 </style>
