9289c3f43a7ce72b2730bf9552dbaaddd1f81e8d
eallenOP <elise.allen@op.ac.nz>
2025-05-22 16:24:36 +1200

    Use required header to fetch from ReqRes API

---
+++ src/routes/employees/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    // The data prop brings anything you fetched in the load function in here for use on the page
+    let { data } = $props();
+    console.log(data); // You can see the data in the console using the browser's dev tools
+</script>
