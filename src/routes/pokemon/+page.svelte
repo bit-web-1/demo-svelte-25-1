fa3dc27231f0405fd973ebaf0c52a7238122f7aa
eallenOP <elise.allen@op.ac.nz>
2025-05-22 15:44:58 +1200

    Display one pokemon in a card

--- src/routes/pokemon/+page.svelte
+++ src/routes/pokemon/+page.svelte
@@ -1,11 +1,22 @@
 <script>
-    // This page will display a bunch of pokemon cards
-    let { data } = $props();
-    console.log(data);
+  import Card from "$lib/Card.svelte";
+
+  // This page will display a bunch of pokemon cards
+  let { data } = $props();
+  const pokemon = data.poke; // Grab the internal object for ease of typing
+  console.log(pokemon); // Just checking
+
+  const pokeData = {
+      title: pokemon.name,
+      user: {
+        userName: pokemon.name,
+        img: pokemon.sprites.front_default,
+      },
+      info: "information here",
+    }
 </script>
 
-
+<Card cardData={pokeData}/>
 
 <style>
-
 </style>
